6369dc589bb27b7a2501cb98437add28e21bf1a4
3aBucTb <Envy5@ukr.net>
2023-03-12 21:41:36 +0200

    1

---
+++ homework lession 3.py
@@ -0,0 +1,21 @@
+love = "I love Python" * 42
+print(love)
+age_in_month = 288
+age_in_year = age_in_month / 12
+name = "Andrey"
+my_age = f"My name is {name} i'am {age_in_year} years old."
+print(my_age)
+y = 1
+print(y == 1)
+print(y < 2)
+print(y > 2)
+print(y >= 3)
+print(y <= 3)
+a = 2
+b = 5
+c = 6
+d = a*b*b*b+c
+print(d)
+
+
+
